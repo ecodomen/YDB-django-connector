804046edabc7edf9642fa2ef3590f73f91f640d6
poligrafo <poligrafo888@gmail.com>
2024-08-15 11:27:52 +0300

    features/edit base.py

--- ydb_adapter/base.py
+++ ydb_adapter/base.py
@@ -1,55 +1,52 @@
 from django.db.backends.base.base import BaseDatabaseWrapper
-from django.db.backends.base.client import BaseDatabaseClient
-from django.db.backends.base.creation import BaseDatabaseCreation
-from django.db.backends.base.introspection import BaseDatabaseIntrospection
-from django.db.backends.base.operations import BaseDatabaseOperations
-from django.db.backends.base.schema import BaseDatabaseSchemaEditor
 import ydb
 
+from ydb_adapter.crud import DatabaseCreation
 from ydb_adapter.features import DatabaseFeatures
+from ydb_adapter.introspection import DatabaseIntrospection
 
 
 class DatabaseWrapper(BaseDatabaseWrapper):
     vendor = 'ydb'
     display_name = 'YDB'
 
-    # Определение классов для использования в обертке
-    # TODO: необходимо реализовать все остальные классы
+    # Define classes to use within the wrapper
     Database = ydb
-    # schema_editor_class = DatabaseSchemaEditor Serge
-    # creation_class = DatabaseCreation
     features_class = DatabaseFeatures
-    # introspection_class = DatabaseIntrospection Serge
-    # operations_class = DatabaseOperations Serge
+    creation_class = DatabaseCreation
+    introspection_class = DatabaseIntrospection
+    # TODO: Implement the remaining necessary classes
+    # schema_editor_class = DatabaseSchemaEditor
+    # operations_class = DatabaseOperations
     # client_class = DatabaseClient
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.connection = None
 
-    def get_connection_params(self):
-        # Получение параметров соединения из настроек
+    def get_connection_params(self) -> dict:
+        """Retrieve connection parameters from settings."""
         settings_dict = self.settings_dict
         return {
             "endpoint": settings_dict['HOST'],
             "database": settings_dict['NAME']
         }
 
-    def get_new_connection(self, conn_params):
-        # Создание нового соединения с YDB
+    def get_new_connection(self, conn_params: dict):
+        """Create a new connection to YDB."""
         return ydb.Driver(endpoint=conn_params['endpoint'], database=conn_params['database'])
 
     def init_connection_state(self):
-        # Инициализация состояния соединения
+        """Initialize connection state."""
         if not self.connection.is_connected:
             self.connection.connect()
 
     def create_cursor(self):
-        # Создание курсора для выполнения запросов
+        """Create a cursor for executing queries."""
         return self.connection.cursor()
 
-    def is_usable(self):
-        # Проверка, является ли соединение используемым
+    def is_usable(self) -> bool:
+        """Check if the connection is usable."""
         try:
             self.connection.ping()
             return True
@@ -57,6 +54,6 @@ class DatabaseWrapper(BaseDatabaseWrapper):
             return False
 
     def close(self):
-        # Закрытие соединения с базой данных
+        """Close the connection to the database."""
         if self.connection is not None:
             self.connection.close()
